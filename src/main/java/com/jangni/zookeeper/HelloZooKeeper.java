aa3ed39e0a4b9e878bf66abf8509064c9aa015ca
Jangni <90706827@163.com>
2019-05-08 23:21:29 +0800

    zookeeper

--- src/main/java/com/jangni/zookeeper/HelloZooKeeper.java
+++ src/main/java/com/jangni/zookeeper/HelloZooKeeper.java
@@ -9,6 +9,10 @@ import org.apache.zookeeper.data.Id;
 import org.apache.zookeeper.server.auth.DigestAuthenticationProvider;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.support.AbstractRefreshableApplicationContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.security.NoSuchAlgorithmException;
@@ -24,9 +28,10 @@ import java.util.Properties;
  * @Date 2019/4/26 21:31
  * @Version 1.0
  **/
-public class HelloZooKeeper {
-    static Logger logger = LoggerFactory.getLogger(HelloZooKeeper.class);
 
+public class HelloZooKeeper {
+
+    static Logger logger = LoggerFactory.getLogger(HelloZooKeeper.class);
     public static void main(String[] args) throws IOException {
 
         Properties config = new Properties();
--- src/main/java/com/jangni/zookeeper/ZookeeperApplication.java
+++ src/main/java/com/jangni/zookeeper/ZookeeperApplication.java
@@ -2,12 +2,20 @@ package com.jangni.zookeeper;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(value = "com.jangni.zookeeper")
 public class ZookeeperApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ZookeeperApplication.class, args);
+
+		ConfigurableApplicationContext context = SpringApplication.run(ZookeeperApplication.class, args);
+		Test test = context.getBean("test", Test.class);
+		System.out.println(test.toString());
 	}
+
 
 }
